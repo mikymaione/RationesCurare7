a9fe984d6d02173d589b305c7be0f314a5f7de37
Michele Maione <mikymaione@hotmail.it>
2019-03-01 10:21:29 +0100

    aggiornato numero di versione

--- RationesCurare7/Properties/AssemblyInfo.cs
+++ RationesCurare7/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("20.19.02.16")]
-[assembly: AssemblyFileVersion("20.19.02.16")]
+[assembly: AssemblyVersion("20.19.03.01")]
+[assembly: AssemblyFileVersion("20.19.03.01")]
