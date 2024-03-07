7afedd02d934f0b9623daf9098bc25a4a80cc12c
Michele Maione <mikymaione@hotmail.it>
2024-03-07 15:54:22 +0100

    feat: set chart custom palette

--- RationesCurare/mGraficoTorta.aspx.cs
+++ RationesCurare/mGraficoTorta.aspx.cs
@@ -4,11 +4,36 @@ using System.Drawing.Text;
 using System.Drawing;
 using System.Data;
 using System.Linq;
+using System.Web.UI.DataVisualization.Charting;
 
 namespace RationesCurare
 {
     public partial class mGraficoTorta : System.Web.UI.Page
     {
+
+        private static Color[] KELLY_COLORS = {
+            ColorTranslator.FromHtml("0xFFB300"),    // Vivid Yellow
+            ColorTranslator.FromHtml("0x803E75"),    // Strong Purple
+            ColorTranslator.FromHtml("0xFF6800"),    // Vivid Orange
+            ColorTranslator.FromHtml("0xA6BDD7"),    // Very Light Blue
+            ColorTranslator.FromHtml("0xC10020"),    // Vivid Red
+            ColorTranslator.FromHtml("0xCEA262"),    // Grayish Yellow
+            ColorTranslator.FromHtml("0x817066"),    // Medium Gray
+
+            ColorTranslator.FromHtml("0x007D34"),    // Vivid Green
+            ColorTranslator.FromHtml("0xF6768E"),    // Strong Purplish Pink
+            ColorTranslator.FromHtml("0x00538A"),    // Strong Blue
+            ColorTranslator.FromHtml("0xFF7A5C"),    // Strong Yellowish Pink
+            ColorTranslator.FromHtml("0x53377A"),    // Strong Violet
+            ColorTranslator.FromHtml("0xFF8E00"),    // Vivid Orange Yellow
+            ColorTranslator.FromHtml("0xB32851"),    // Strong Purplish Red
+            ColorTranslator.FromHtml("0xF4C800"),    // Vivid Greenish Yellow
+            ColorTranslator.FromHtml("0x7F180D"),    // Strong Reddish Brown
+            ColorTranslator.FromHtml("0x93AA00"),    // Vivid Yellowish Green
+            ColorTranslator.FromHtml("0x593315"),    // Deep Yellowish Brown
+            ColorTranslator.FromHtml("0xF13A13"),    // Vivid Reddish Orange
+            ColorTranslator.FromHtml("0x232C16"),    // Dark Olive Green
+        };
 
         private DateTime CurrentData
         {
@@ -54,6 +79,9 @@ namespace RationesCurare
             Chart1.Series[0]["PieLabelStyle"] = "Disabled";
             Chart1.Legends[0].Font = ubuntuFont;
 
+            Chart1.Palette = ChartColorPalette.None;
+            Chart1.PaletteCustomColors = KELLY_COLORS;
+
             if (!IsPostBack)
                 CurrentData = GB.DateStartOfMonth(DateTime.Now);
 
@@ -95,7 +123,7 @@ namespace RationesCurare
                         where
                             r[1] as double? > 0
                         orderby
-                            r[1] descending
+                            r[1] as double? descending
                         select
                             r;
 
@@ -104,13 +132,13 @@ namespace RationesCurare
                         where
                             r[1] as double? <= 0
                         orderby
-                            r[1] ascending
+                            r[1] as double? ascending
                         select
                             r;
 
-                    var sorted = pos.Union(neg);
-
-                    Chart1.DataSource = sorted.CopyToDataTable();
+                    Chart1.DataSource = pos
+                        .Union(neg)
+                        .CopyToDataTable();
                 }
 
                 Chart1.DataBind();
