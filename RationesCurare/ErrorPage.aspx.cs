ec94cbdab534e4fa9e2032369ed1d7b8af91f380
mikymaione <mikymaione@hotmail.it>
2024-09-08 17:39:14 +0200

    justify error message

--- RationesCurare/ErrorPage.aspx.cs
+++ RationesCurare/ErrorPage.aspx.cs
@@ -12,7 +12,7 @@ namespace RationesCurare
             if (ex != null)
             {
                 ErrorMessageLiteral.Text = $"<h4>Error Message</h4><p>{ex.Message}</p>";
-                ErrorDetailLiteral.Text = $"<h4>Error Details</h4><p><small>{ex.StackTrace}</small></p>";
+                ErrorDetailLiteral.Text = $"<h4>Error Details</h4><p class='noGiustificato'><small>{ex.StackTrace}</small></p>";
             }
         }
 
